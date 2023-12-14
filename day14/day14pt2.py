db7acf00651ca4b21b7cd1886200c8bbbb03cb85
tipx2 <tipx2gaming@gmail.com>
2023-12-14 20:10:57 +0000

    day 14 done

--- day14/day14pt2.py
+++ day14/day14pt2.py
@@ -1,61 +1,41 @@
-with open("day14/test14.txt") as f:
-  lines = f.readlines()
+with open("day14/input14.txt") as f:
+  lines = [x.strip() for x in f.readlines()]
 
-oRocksArr = []
-hashRocksArr = []
+def moveAll(lines):
+  for i in range(len(lines)):
+    while lines[i] != lines[i].replace("O.", ".O"):
+      lines[i] = lines[i].replace("O.", ".O")
+  return lines
+  
+def rotate(lines):
+  return list(map("".join, zip(*reversed(lines))))
 
-for i in range(len(lines)):
-  for j in range(len(lines[i])):
-    if lines[i][j] == "O":
-      oRocksArr.append((i, j))
-    elif lines[i][j] == "#":
-      hashRocksArr.append((i, j))
+def cycle(l):
+  for _ in range(4):
+    l = moveAll(rotate(l))
+  return l
 
-
-def moveAll(t):
-  for i in range(len(oRocksArr)):
-    while True:
-      tempcoord = tuple([oRocksArr[i][0] + t[0], oRocksArr[i][1] + t[1]])
-      withinlims = (0 <= tempcoord[0] <= len(lines)) and (0 <= tempcoord[1] <= len(lines[0]))
-      if withinlims and tempcoord not in hashRocksArr and tempcoord not in oRocksArr:
-          oRocksArr[i] = tempcoord
-      else:
-          break
-
-def cycle():
-  # north
-  moveAll((-1, 0))
-  # west
-  moveAll((0, -1))
-  # south
-  moveAll((1, 0))
-  # east
-  moveAll((0, 1))
-
-def getLoad():
+def getLoad(l):
   total = 0
-  for i in range(len(oRocksArr)):
-    total += len(lines) - oRocksArr[i][0]
+  for i in range(len(l)):
+    total += l[i].count("O") * (len(l) - i)
   return total
 
-counter = 0
 states = {}
-
-while True:
+for k in range(1_000_000_000):
+  lines = cycle(lines)
+  currstate = "".join(lines)
   
-  state = tuple(oRocksArr)
-  if state not in states.keys():
-    states[state] = counter
-  else:
-    cyc = counter - states[state]
+  if currstate in states.keys():
+    loop_size = k - states[currstate]
+    loop_start = states[currstate]
     break
+  else:
+    states[currstate] = k
 
-  cycle()
-  counter += 1
-  
+step = (1_000_000_000 - (loop_start + 1)) % loop_size
 
+for j in range(step):
+  lines = cycle(lines)
 
-for x in range(1000000000%cyc - 1):
-  cycle()
-
-print(getLoad())
+print(getLoad(lines))
