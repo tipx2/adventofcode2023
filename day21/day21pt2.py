c8822b8d21b2de39447a1d221d4a8d845bc6ba3f
tipx2 <tipx2gaming@gmail.com>
2023-12-25 07:26:19 +0000

    finished off day 21 pt 2

--- day21/day21pt2.py
+++ day21/day21pt2.py
@@ -26,22 +26,31 @@ def debugDraw(rocks, walk_positions):
         print(".", end="")
     print()
   
-
-# each step, look in all 4 directions of each current step and add them all to a new array if they are not a rock
-# then set walkable positions to that array
-# repeat for n times
-
-for x in range(2319084320989502980):
+quad = []
+for x in range(1, 2319084320989502980):
   new_positions = []
   for position in walkable_positions:
     for d in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
       new_pos = (position[0] + d[0], position[1] + d[1])
-      if (0 <= new_pos[0] < rowlen) and (0 <= new_pos[1] < collen) and ((new_pos[0] % rowlen,  new_pos[1] % collen) not in rocks):
+      if (new_pos[0] % rowlen,  new_pos[1] % collen) not in rocks:
         new_positions.append(new_pos)
-  
-  
+
   walkable_positions = list(set(new_positions))
+  
+  if x%collen == 26501365%collen:
+    quad.append(len(walkable_positions))
+    if len(quad) == 3:
+      break
 
-#debugDraw(rocks, walkable_positions)
+# assemble quadratic below
+def f(n):
+  a0 = quad[0]
+  a1 = quad[1]
+  a2 = quad[2]
+  
+  b0 = a0
+  b1 = a1-a0
+  b2 = a2-a1
+  return b0 + b1*n + (n*(n-1)//2)*(b2-b1)
 
-print(len(walkable_positions))
+print(f(26501365//collen))
